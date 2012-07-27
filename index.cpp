2ccd3104e5f42f706b7528699b28f21d57a8ed6b
Daniel Keep <daniel.keep@gmail.com>
2012-07-27 00:40:51 -0700

    Updated page with link back to public repo.
    I should probably swap it around so that the working source is in another
    directory and the live site is pulled down from github.  But blah, I can't be
    arsed.

--- index.cpp
+++ index.cpp
@@ -14,14 +14,28 @@ using std::endl;
 // *********************************************************************** //
 // *********************************************************************** //
 
+#define REPOSITORY u8"https://github.com/DanielKeep/danielkeep.koding.com"
+#define BLOB_PREFIX u8"/blob/master/"
+
+// *********************************************************************** //
+// *********************************************************************** //
+
 int main(int argc, const char **argv)
 {
-    cout << u8"Content-Type: text/html; charset=utf-8" << endl;
-    cout << endl;
+    cout
+        << u8"Content-Type: text/html; charset=utf-8" << endl
+        << u8"X-Source-Repository: " << REPOSITORY << endl
+        << endl;
+
     cout
         << u8"<!DOCTYPE html>" << endl
         << u8"<html>" << endl
-        << u8"<body><h1>Hello, World!</h1></body>" << endl
+        << u8"<body>" << endl
+        << u8"<h1>Hello, World!</h1>" << endl
+        << u8"<p>The <a href=\""
+            << REPOSITORY BLOB_PREFIX __FILE__
+            << "\">page source code</a> (possibly not live).</p>" << endl
+        << u8"</body>" << endl
         << u8"</html>" << endl;
 
     return 0;
